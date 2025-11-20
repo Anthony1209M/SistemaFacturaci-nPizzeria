b8128c6713d764b0c410d7fe45ab6f3a7b465460
Anthony1209M <elincreible2009@gmail.com>
2025-11-20 01:02:26 -0400

    Add files via upload

---
+++ src/modelos/Administrador.java
@@ -0,0 +1,9 @@
+package modelos;
+
+public class Administrador extends Usuario 
+{
+         public Administrador(int id, String nombre, String apellido, String telefono, String rol)
+         {
+        	 super(id, nombre, apellido, telefono, rol);
+         }
+}
---
+++ src/modelos/UsuarioRegular.java
@@ -0,0 +1,10 @@
+package modelos;
+
+public class UsuarioRegular extends Usuario
+{
+	public UsuarioRegular(int id, String nombre, String apellido, String telefono, String rol)
+    {
+   	 super(id, nombre, apellido, telefono, rol);
+    }
+
+}
